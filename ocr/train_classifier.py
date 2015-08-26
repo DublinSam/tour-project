0ac919a5c9b0b64f69b9c2020ebf43583885f25e
sam <satiricallaughter@gmail.com>
2015-08-26 13:14:33 +0100

    updated utils

--- ocr/train_classifier.py
+++ ocr/train_classifier.py
@@ -3,12 +3,14 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.patches as patches
+from collections import Counter
 
 from utils import crop_frame
 from utils import find_contours
 from utils import border_rectangle
 from utils import get_fig_dimensions
 from utils import apply_threshold_to_image
+from distance_labels import find_sign_location
 
 # Path to data (sample frames from sports footage)
 DATA_PATH = '/Users/samuelalbanie/aims_course/project_two/code/tour_data/project_tools/ocr/data'
@@ -34,22 +36,43 @@ def get_jpgs_in_dir(image_dir):
         root = root
     return root, frames
 
-def construct_training_image(src_dir=SAMPLE_IMAGES, target_dir=TARGET_DIR):
+def show_training_digits_distribution(src_dir):
+    path, jpgs = get_jpgs_in_dir(src_dir)
+    file_list = "".join([img_name[:-4] for img_name in jpgs])
+    counter = Counter(file_list)
+    digits, counts = [],[]
+    for digit in sorted(counter):
+        digits.append(digit)
+        counts.append(counter[digit])
+
+    indexes = np.arange(len(digits))
+    width = 1
+    plt.bar(indexes, counts, alpha=0.6)
+    plt.title('Distribution of training digits')
+    plt.xlabel('digit')
+    plt.ylabel('counts')
+    plt.xticks(indexes + width * 0.5, digits)
+    plt.show()
+
+def construct_training_image(src_dir, target_dir, template, sign_region, sign_width, sign_height):
     """constructs an image containing at least one example of 
     each digit based on the files in src_dir and saves it as 
-    'digit_examples.jpg' in target_dir.  Saving the image isn't 
+    'digit_examples.jpg' in target_dir.  The digits are found in 
+    the region specified by sign_region. Saving the image isn't 
     necessary, but it provides a useful sanity check."""
     root, frame_paths = get_jpgs_in_dir(src_dir)
 
     # Set up pyplot figure to draw sample digits onto
     fig, axes = plt.subplots(len(frame_paths),1)
-    fig_width, fig_height = get_fig_dimensions()
+    fig_width, fig_height = get_fig_dimensions(sign_width, sign_height)
     fig.set_size_inches(fig_width, len(frame_paths) * fig_height)
     fig.subplots_adjust(hspace=0, wspace=0)
     plt.axis('off')
+
     for frame, ax in zip(frame_paths, axes):
         img = cv2.imread(root + frame, cv2.IMREAD_GRAYSCALE)
-        km_img = crop_frame(img)
+        crop_region = find_sign_location(root + frame, template, sign_region, sign_width, sign_height)
+        km_img = crop_frame(img, crop_region)
         rectangle = border_rectangle(km_img)
         ax.imshow(km_img, cmap = plt.cm.Greys_r)
         ax.add_patch(patches.Rectangle(**rectangle))
--- ocr/utils.py
+++ ocr/utils.py
@@ -1,15 +1,6 @@
 import cv2
 import numpy as np
 
-# Hardcoded values for the location of the 'km to go' sign
-# (that need to be updated for each new piece of footage)
-CROP_FRAME = {
-    'top_left_x': 169,
-    'top_left_y': 23,
-    'bottom_right_x': 217,
-    'bottom_right_y': 39
-}
-
 def apply_threshold_to_image(path):
     """apply a uniform threshold to convert the image located at
     `path` to binary.  he binary image is then inverted to help 
@@ -27,19 +18,17 @@ def find_contours(binary_img):
                                        method=cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
-def crop_frame(frame):
-    """returns cropped frame using the values provided at
-     the top of the file."""
-    tl_x = CROP_FRAME['top_left_x'] 
-    tl_y = CROP_FRAME['top_left_y']
-    br_x = CROP_FRAME['bottom_right_x']
-    br_y = CROP_FRAME['bottom_right_y']
+def crop_frame(frame, crop_region):
+    """returns cropped frame using the values specified by
+     'crop_region'."""
+    tl_x = crop_region['top_left_x'] 
+    tl_y = crop_region['top_left_y']
+    br_x = crop_region['bottom_right_x']
+    br_y = crop_region['bottom_right_y']
     return frame[tl_y:br_y, tl_x:br_x]
 
-def get_fig_dimensions():
-    """Hardcoded dimensions based on CROP_FRAME values"""
-    width = CROP_FRAME['bottom_right_x'] - CROP_FRAME['top_left_x']
-    height = CROP_FRAME['bottom_right_y'] - CROP_FRAME['top_left_y']
+def get_fig_dimensions(width, height):
+    """return normalized fig dimensions"""
     aspect_ratio = width / height
     fig_height = 1
     fig_width = aspect_ratio * fig_height
