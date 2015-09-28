b13112eb1ef6d3e70862aca980e5cbfd3551b728
sam <satiricallaughter@gmail.com>
2015-09-28 18:38:10 +0100

    added camera init test

---
+++ tests/distances/test_camera.py
@@ -0,0 +1,25 @@
+"""Add parent directory to path"""
+import os,sys,inspect
+currentdir_loc = os.path.abspath(inspect.getfile(inspect.currentframe()))
+currentdir = os.path.dirname(currentdir_loc)
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+import unittest
+import cv2
+
+from test_settings import ROOT_PATH
+from camera import CameraFocus
+
+class TestCamera(unittest.TestCase):
+
+    def test_camera_initializes_correctly(self):
+        """check that load_model returns an instance of 
+        a kNN classifier."""
+        stage_id = 1
+        camera_focus = CameraFocus(ROOT_PATH, stage_id)
+        expected_num_precis_frames = 1582
+        num_precis_frames = len(camera_focus.frames)
+        self.assertEqual(expected_num_precis_frames, num_precis_frames)
+
+if __name__ == "__main__":
+    unittest.main()
