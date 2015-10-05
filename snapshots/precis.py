b0ffd95462f53570220e9e3a5b3c938c807c6f4a
sam <satiricallaughter@gmail.com>
2015-10-05 13:56:38 +0100

    added snapshot updates

--- snapshots/precis.py
+++ snapshots/precis.py
@@ -7,11 +7,15 @@ from file_utils import get_target_dir
 from time_utils import formatted_times_in_video
 from snapshot_utils import get_dar_dimensions
 from snapshot_utils import snapshot
+from file_utils import get_paths
 
-def take_precis_snapshots(src_video, target_dir, stage_id, DAR):
+def take_precis_snapshots(root_path, stage_id, DAR=(16.0/9.0)):
     """saves a snapshot of src_video every second 
     and saves it to the target_dir."""
     DAR = DAR
+    paths = get_paths(root_path, stage_id)
+    src_video = paths['src_video']
+    target_dir = paths['precis']
     dar_dims = get_dar_dimensions(src_video, DAR=DAR)
     times = formatted_times_in_video(src_video)
     if not os.path.exists(target_dir):
--- snapshots/tete_frames.py
+++ snapshots/tete_frames.py
@@ -8,6 +8,8 @@ from file_utils import get_img_paths_in_dir
 from time_utils import get_time_from_path
 from time_utils import get_times_in_interval
 from time_utils import get_contiguous_intervals
+from snapshot_utils import snapshot
+from snapshot_utils import get_dar_dimensions
 from snapshot_cluster import take_cluster_snapshots_at_targets
 from file_utils import get_paths
 
@@ -17,15 +19,26 @@ def get_tete_images(paths):
     img_names = get_img_paths_in_dir(paths['precis'])
     labeled_imgs = zip(camera_states_log, sorted(img_names))
     tete_imgs = [img for (state, img) in labeled_imgs if state == Camera.Tete]
-    return tete_imgs   
+    return tete_imgs
 
-def get_tete_target_frames(paths, step):
+def get_tete_intervals(paths):
     tete_imgs = get_tete_images(paths)
     times = [get_time_from_path(tete_img) for tete_img in tete_imgs]
     intervals = get_contiguous_intervals(times)
+    return intervals   
+
+def get_tete_target_frames(paths, step):
+    intervals = get_tete_intervals(paths)
     target_lists = [get_times_in_interval(interval, step) for interval in intervals]
     targets = sum(target_lists, []) # flattens the list
     return targets
+
+def extract_all_tete_frames(root_path, stage_id, step=40, DAR=(16.0 / 9.0)):
+    paths = get_paths(root_path, stage_id)
+    targets = get_tete_target_frames(paths, step)
+    dar_dims = get_dar_dimensions(paths['src_video'], DAR=DAR)
+    for time in tqdm(targets):
+        snapshot(paths['src_video'], paths['tete'], stage_id, time=time, dimensions=dar_dims)
 
 def extract_tete_snapshots(root_path, stage_id, step):
     paths = get_paths(root_path, stage_id)
