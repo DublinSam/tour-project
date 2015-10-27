dca0faf7b7ef8a404d399c507ea511284dd3c884
sam <satiricallaughter@gmail.com>
2015-10-27 10:39:34 +0000

    fused camera focus and tete snapshots

--- titan-scripts/tete_snapshots.py
+++ titan-scripts/tete_snapshots.py
@@ -8,6 +8,11 @@ as an input argument to define the stage number."""
 stage = sys.argv[1]
 stage_id = str(stage)
 
+"""First extract  the camera focus for each stage"""
+camera_focus = CameraFocus(root_path, stage_id)
+camera_focus.get_camera_states()
+camera_focus.save_camera_states()
+
 """set the Display Aspect Ratio for snapshots."""
 step = 500 # in milliseconds
 extract_tete_snapshots(root_path, stage_id, step)
