a4b233594d76020edea72efdd12d3c670f971bf6
sam <satiricallaughter@gmail.com>
2015-11-20 10:14:55 +0000

    added ocr tests

--- tests/ocr/test_digit_classifier.py
+++ tests/ocr/test_digit_classifier.py
@@ -5,11 +5,20 @@ currentdir = os.path.dirname(currentdir_loc)
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
 import unittest
+import matplotlib
+matplotlib.use('Agg')
 import cv2
 
 from digit_classifier import load_model
+from file_utils import get_img_paths_in_dir
 from file_utils import get_paths
 from test_settings import ROOT_PATH
+from digit_classifier import find_number
+from extract_cyclist_faces import load_cache
+
+def get_distance_from_filename(img_name):
+    num = img_name.split('/')[-1][:-4]
+    return num
 
 class TestDigitClassifier(unittest.TestCase):
 
@@ -20,6 +29,27 @@ class TestDigitClassifier(unittest.TestCase):
         model = load_model(paths)
         expected_type = type(cv2.KNearest())
         self.assertEqual(type(model), expected_type)
+
+    def test_classifier_accuracy(self):
+        """check that the classifier is accurate."""
+        paths = get_paths(ROOT_PATH, "1")
+        img_names = get_img_paths_in_dir(paths['digit_testing_frames'])
+        cache = load_cache(paths)
+        test_nums = [get_distance_from_filename(img_name) for img_name in img_names]
+        test_pairs = [(name, target) for (name, target) in zip(img_names, test_nums)]
+        accuracy = 0
+        for img_name, target in test_pairs:
+            number = find_number(img_name, paths, cache['model'], cache['templates'])
+            full_num = str(number).replace(".", "")
+            if full_num == target:
+                accuracy += 1
+            else:
+                print(img_name)
+                print(full_num)
+                print('-------')
+        expected_accuracy = 100
+        self.assertEqual(expected_accuracy, accuracy)
+
 
 if __name__ == "__main__":
     unittest.main()
