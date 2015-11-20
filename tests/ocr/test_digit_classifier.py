1ad10adc81c165cc3ed2af895f741a0e1886054f
sam <satiricallaughter@gmail.com>
2015-11-20 11:45:56 +0000

    updated tests for 2013

--- tests/ocr/test_digit_classifier.py
+++ tests/ocr/test_digit_classifier.py
@@ -12,13 +12,32 @@ import cv2
 from digit_classifier import load_model
 from file_utils import get_img_paths_in_dir
 from file_utils import get_paths
-from test_settings import ROOT_PATH_2012
+from test_settings import ROOT_PATH_2012, ROOT_PATH_2013
 from digit_classifier import find_number
 from extract_cyclist_faces import load_cache
 
 def get_distance_from_filename(img_name):
     num = img_name.split('/')[-1][:-4]
     return num
+
+def find_classifier_accuracy(root_path):
+    """check that the classifier is accurate."""
+    paths = get_paths(root_path, "1")
+    img_names = get_img_paths_in_dir(paths['digit_testing_frames'])
+    cache = load_cache(paths)
+    test_nums = [get_distance_from_filename(img_name) for img_name in img_names]
+    test_pairs = [(name, target) for (name, target) in zip(img_names, test_nums)]
+    accuracy = 0
+    for img_name, target in test_pairs:
+        number = find_number(img_name, paths, cache['model'], cache['templates'])
+        full_num = str(number).replace(".", "")
+        if full_num == target:
+            accuracy += 1
+        else:
+            print(img_name)
+            print(full_num)
+            print('-------')
+    return accuracy
 
 class TestDigitClassifier(unittest.TestCase):
 
@@ -30,23 +49,17 @@ class TestDigitClassifier(unittest.TestCase):
         expected_type = type(cv2.KNearest())
         self.assertEqual(type(model), expected_type)
 
-    def test_classifier_accuracy(self):
-        """check that the classifier is accurate."""
-        paths = get_paths(ROOT_PATH_2012, "1")
-        img_names = get_img_paths_in_dir(paths['digit_testing_frames'])
-        cache = load_cache(paths)
-        test_nums = [get_distance_from_filename(img_name) for img_name in img_names]
-        test_pairs = [(name, target) for (name, target) in zip(img_names, test_nums)]
-        accuracy = 0
-        for img_name, target in test_pairs:
-            number = find_number(img_name, paths, cache['model'], cache['templates'])
-            full_num = str(number).replace(".", "")
-            if full_num == target:
-                accuracy += 1
-            else:
-                print(img_name)
-                print(full_num)
-                print('-------')
+    def test_2012_classifier_accuracy(self):
+        """check classifier accuracy for the 2012 dataset."""
+        root_path = ROOT_PATH_2012
+        accuracy = find_classifier_accuracy(root_path)
+        expected_accuracy = 100
+        self.assertEqual(expected_accuracy, accuracy)
+
+    def test_2013_classifier_accuracy(self):
+        """check classifier accuracy for the 2013 dataset."""
+        root_path = ROOT_PATH_2013
+        accuracy = find_classifier_accuracy(root_path)
         expected_accuracy = 100
         self.assertEqual(expected_accuracy, accuracy)
 
